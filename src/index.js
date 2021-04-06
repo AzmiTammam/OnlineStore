290667501620f30853d5c7fbad657aa729e5f124
AzmiTammam <azmitammam@gmail.com>
2021-04-06 22:49:46 +0300

    two change

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/style.css';
+
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
